d74b02e568177a808a70bc9310a7c71b70b21b43
Marat-terabyte <marat.amzph21@gmail.com>
2023-04-01 21:27:16 +0300

    Add fields

--- ToDo-List/Models/Task.cs
+++ ToDo-List/Models/Task.cs
@@ -8,5 +8,11 @@ namespace ToDo_List.Models
 {
     internal class Task
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public bool IsCompleted { get; set; }
     }
 }
