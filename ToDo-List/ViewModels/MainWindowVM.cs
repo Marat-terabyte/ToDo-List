223d003e8bc809641d9d6d290c3b7f36180f1320
Marat-terabyte <marat.amzph21@gmail.com>
2023-04-01 21:31:01 +0300

    Temporary solution (list of tasks)

--- ToDo-List/ViewModels/MainWindowVM.cs
+++ ToDo-List/ViewModels/MainWindowVM.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDo_List.Models;
 
 namespace ToDo_List.ViewModels
 {
     internal class MainWindowVM
     {
+        public ObservableCollection<Models.Task> Tasks { get; set; }
+
+        public MainWindowVM()
+        {
+            Tasks = new ObservableCollection<Models.Task>
+            {
+                new Models.Task { Id = 1, Title = "Доделать программу", Description = "...", StartTime = "01.04.2023", EndTime = "15.03.2023", IsCompleted = false },
+                new Models.Task { Id = 2, Title = "Доделать программу", Description = "...", StartTime = "01.04.2023", EndTime = "15.03.2023", IsCompleted = true },
+                new Models.Task { Id = 3, Title = "Доделать программу", Description = "...", StartTime = "01.04.2023", EndTime = "15.03.2023", IsCompleted = true }
+            };
+        }
     }
 }
