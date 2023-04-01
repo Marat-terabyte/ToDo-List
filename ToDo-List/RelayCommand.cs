4e738c55be91740a0be6ecf1db2d06797f5a909e
Marat-terabyte <marat.amzph21@gmail.com>
2023-04-01 15:48:17 +0300

    Add

---
+++ ToDo-List/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ToDo_List
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action<object> _command;
+
+        public RelayCommand(Action<object> action)
+        {
+            _command = action;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _command(parameter);
+        }
+    }
+}
