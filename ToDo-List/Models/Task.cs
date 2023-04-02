b647dbc93154b771334fe41680989528527bd40f
Marat-terabyte <marat.amzph21@gmail.com>
2023-04-02 13:44:34 +0300

    Release INotifyPropertyChanged

--- ToDo-List/Models/Task.cs
+++ ToDo-List/Models/Task.cs
@@ -1,18 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ToDo_List.Models
 {
-    internal class Task
+    internal class Task : INotifyPropertyChanged
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string StartTime { get; set; }
-        public string EndTime { get; set; }
-        public bool IsCompleted { get; set; }
+        #region Fields
+        private int _id;
+        private string _title;
+        private string _description;
+        private string _startTime;
+        private string _endTime;
+        private bool _isCompleted;
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public string StartTime
+        {
+            get { return _startTime; }
+            set
+            {
+                _startTime = value;
+                OnPropertyChanged("StartTime");
+            }
+        }
+        public string EndTime
+        {
+            get { return _endTime; }
+            set
+            {
+                _endTime = value;
+                OnPropertyChanged("EndTime");
+            }
+        }
+        public bool IsCompleted
+        {
+            get { return _isCompleted; }
+            set
+            {
+                _isCompleted = value;
+                OnPropertyChanged("IsCompleted");
+            }
+        }
+        #endregion
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
