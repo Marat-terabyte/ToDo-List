efd85b31bc0f91623de0614de3cef6dae9ffe277
Marat-terabyte <marat.amzph21@gmail.com>
2023-04-04 16:23:52 +0300

    Add method OpenAddTaskWindow

--- ToDo-List/ViewModels/MainWindowVM.cs
+++ ToDo-List/ViewModels/MainWindowVM.cs
@@ -6,11 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using ToDo_List.Models;
+using ToDo_List.Views;
 
 namespace ToDo_List.ViewModels
 {
     internal class MainWindowVM
     {
+        public RelayCommand OpenAddTaskWindowCommand { get; }
         public ObservableCollection<Models.Task> Tasks { get; set; }
 
         public string DateTimeNow
@@ -25,10 +27,14 @@ namespace ToDo_List.ViewModels
         {
             Tasks = new ObservableCollection<Models.Task>
             {
-                new Models.Task { Id = 1, Title = "Доделать программу", Description = "...", StartTime = "01.04.2023", EndTime = "15.03.2023", IsCompleted = false },
+                new Models.Task { Id = 1, Title = "Make program", Description = "...", StartTime = "01.04.2023", EndTime = "15.03.2023", IsCompleted = false },
                 new Models.Task { Id = 2, Title = "Доделать программу", Description = "...", StartTime = "01.04.2023", EndTime = "15.03.2023", IsCompleted = true },
                 new Models.Task { Id = 3, Title = "Доделать программу", Description = "...", StartTime = "01.04.2023", EndTime = "15.03.2023", IsCompleted = true }
             };
+
+            OpenAddTaskWindowCommand = new RelayCommand(o => OpenAddTaskWindow());
         }
+
+        private void OpenAddTaskWindow() => new AddTask().Show();
     }
 }
