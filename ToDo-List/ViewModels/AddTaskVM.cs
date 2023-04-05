5ae9b4c66c81caded4c8ce5057128933ed4d451f
Marat-terabyte <marat.amzph21@gmail.com>
2023-04-05 21:36:20 +0300

    Add method AddTask

--- ToDo-List/ViewModels/AddTaskVM.cs
+++ ToDo-List/ViewModels/AddTaskVM.cs
@@ -1,19 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using ToDo_List.Views;
 
 namespace ToDo_List.ViewModels
 {
     class AddTaskVM
     {
-        public Models.Task Task { get; set; }
-        public DateTime DateTimeNow { get; }        
+        private ObservableCollection<Models.Task> _tasks;
+        private AddTask _window;
 
-        public AddTaskVM()
+        public Models.Task Task { get; set; }
+        public DateTime DateTimeNow { get; } 
+        public RelayCommand AddTaskCommand { get; }
+
+        public AddTaskVM(ObservableCollection<Models.Task> tasks, AddTask window)
         {
+            _tasks = tasks;
+            _window = window;
+
+            Task = new Models.Task();
             DateTimeNow = DateTime.Now;
+            
+            AddTaskCommand = new RelayCommand(o => AddTask());
+        }
+
+        public void AddTask()
+        {
+            if (!string.IsNullOrEmpty(Task.Title))
+            {
+                Task.Id = _tasks.Last().Id + 1;
+                _tasks.Add(Task);
+                _window.Close();
+            }
         }
     }
 }
