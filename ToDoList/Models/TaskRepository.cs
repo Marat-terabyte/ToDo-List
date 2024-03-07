7ef8724c64f39c54a81f1df9327de8845a2f4302
Marat-terabyte <marat.amzph21@gmail.com>
2024-03-07 15:57:05 +0300

    Added Models

---
+++ ToDoList/Models/TaskRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net.Sockets;
+using ToDoList_DatabaseTest.Model.Database;
+
+namespace ToDoList_DatabaseTest.Model
+{
+    /// <summary>
+    /// <see cref="TaskRepository"/> encapsulates the logic of the <see cref="DatabaseContext"/>
+    /// </summary>
+    internal class TaskRepository : IRepository<ToDoTask>
+    {
+        private DatabaseContext _db;
+
+        public TaskRepository()
+        {
+            _db = new DatabaseContext();
+        }
+
+        public IEnumerable<ToDoTask> GetAll()
+        {
+            return _db.Tasks.ToList();
+        }
+
+        public void Add(ToDoTask item)
+        {
+            _db.Tasks.Add(item);
+        }
+
+        public void Delete(int id)
+        {
+            ToDoTask? taskToDelete = _db.Tasks.FirstOrDefault(x => x.Id == id);
+            if (taskToDelete != null)
+            {
+                _db.Tasks.Remove(taskToDelete);
+            }
+        }
+
+        public ToDoTask? Get(int id)
+        {
+            return _db.Tasks.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(ToDoTask item)
+        {
+            _db.Entry(item).State = EntityState.Modified;
+        }
+    }
+}
---
+++ ToDoList/Models/ToDoTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList_DatabaseTest.Model
+{
+    internal class ToDoTask
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? DateLimit {  get; set; }
+
+        public ToDoTask(string name, string? description, DateTime created, DateTime? dateLimit)
+        {
+            Name = name;
+            Description = description;
+            Created = created;
+            DateLimit = dateLimit;
+        }
+
+        public ToDoTask(string name, DateTime created)
+        {
+            Name = name;
+            Created = created;
+        }
+    }
+}
