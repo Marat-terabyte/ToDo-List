766dc6fed3e5e9abc8c4c1009f5c55233425d1cb
Marat-terabyte <marat.amzph21@gmail.com>
2024-03-10 21:05:51 +0300

    Resource dictionaries added

---
+++ ToDoList/ViewModels/MainWindowVM.cs
@@ -0,0 +1,51 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ToDoList.Command;
+using ToDoList.Views;
+using ToDoList_DatabaseTest.Model;
+
+namespace ToDoList.ViewModels
+{
+    internal class MainWindowVM
+    {
+        private IRepository<ToDoTask> _database;
+
+        public string DateTimeNow { get => DateTime.Now.ToString("D"); }
+        public ObservableCollection<ToDoTask> Tasks { get; }
+        
+        public ICommand EditTaskCommand { get; }
+        public ICommand OpenAddTaskCommand { get; }
+        public ICommand DeleteTaskCommand { get; }
+
+        public MainWindowVM()
+        {
+            _database = new TaskRepository();
+            Tasks = new ObservableCollection<ToDoTask>(_database.GetAll());
+            
+            OpenAddTaskCommand = new RelayCommand(o => OpenAddTaskWindow());
+            DeleteTaskCommand = new RelayCommand(o => DeleteTask((int) o));
+            EditTaskCommand = new RelayCommand(o => MessageBox.Show("Edit"));
+        }
+
+        private void DeleteTask(int id)
+        {
+            MessageBox.Show("fsa");
+            Tasks.RemoveAt(id - 1);
+            _database.Delete(id);
+        }
+
+        private void OpenAddTaskWindow()
+        {
+            AddTaskWindow window = new AddTaskWindow(Tasks, _database);
+            window.ShowDialog();
+        }
+    }
+}
--- ToDoList/Views/AddTaskWindow.xaml.cs
+++ ToDoList/Views/AddTaskWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,17 +11,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ToDoList.ViewModels;
 using ToDoList.WindowManagment;
+using ToDoList_DatabaseTest.Model;
 
-namespace ToDoList
+namespace ToDoList.Views
 {
-    public partial class MainWindow : Window
+    public partial class AddTaskWindow : Window
     {
-        public MainWindow()
+        public AddTaskWindow(ObservableCollection<ToDoTask> tasks, IRepository<ToDoTask> db)
         {
             InitializeComponent();
+            DataContext = new AddTaskVM(tasks, db, this);
         }
 
         private void MoveWindow(object sender, MouseButtonEventArgs e) => WindowActions.MoveWindow(this);
